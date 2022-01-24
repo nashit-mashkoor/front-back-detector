a56cde4528957b268ae841741a4fd43c356adfb4
Nashit <ignitorkhan@gmail.com>
2022-01-25 03:35:44 +0500

    Final commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+import io
+
+st.title('Card Front-Back Detector')
+model = load_model('keras_model.h5')
+img_class = ['Front', 'Back']
+prediction = 'No file selected'
+st.header('Upload Image...')
+uploaded_file = st.file_uploader("", type=['png', 'jpeg', 'jpg'])
+if uploaded_file is not None:
+    with st.spinner('Wait for prediction...'):
+        bytes_data = uploaded_file.getvalue()
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        # Replace this with the path to your image
+        image = Image.open(io.BytesIO(bytes_data))
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # run the inference
+        prediction = img_class[np.argmax((model.predict(data)))] + 'side of the card'
+
+    st.header('Results:')
+    st.success('Image Processed!')
+    st.image(image_array)
+    st.text(prediction)
